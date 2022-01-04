7df25c537c15a5f5b824a8e3c5328e10b1292e62
haleywitherell <hwitherell22@gmail.com>
2022-01-03 20:35:12 -0500

    working on resolvers, not done yet tho

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,59 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+
+        if(context.user) {
+            const userData = await User.findOne({_id: context.user._id}).select('-__v -password');
+            return userData;
+        }
+
+        throw new AuthenticationError('Please make sure you are logged in!')
+    },
+  },
+
+//   help
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { thoughtText, thoughtAuthor }) => {
+      const thought = await Thought.create({ thoughtText, thoughtAuthor });
+
+      await User.findOneAndUpdate(
+        { username: thoughtAuthor },
+        { $addToSet: { thoughts: thought._id } }
+      );
+
+      return thought;
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      return Book.findOneAndDelete({ _id: bookId });
+    },
+  },
+};
+
+module.exports = resolvers;
