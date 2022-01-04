0baaba8e905b5f350ce532592a7108b44928add3
haleywitherell <hwitherell22@gmail.com>
2022-01-03 20:37:36 -0500

    updates server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
 }
 
 app.use(routes);
